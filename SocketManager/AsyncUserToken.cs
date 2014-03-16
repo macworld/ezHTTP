9ead243a7ff2fded0f7f13218a48f9f054bb884f
Wenhao Liu <lwh.scu@gmail.com>
2014-03-16 19:15:55 +0800

    Add project framework & SocketManager Module Source code of the SocketManager  is from [MSDN Socket Performance Technology Sample] (http://goo.gl/ElRLZh) with slight changes. Last commit missed many files :(

---
+++ SocketManager/AsyncUserToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace SocketManager
+{
+    public class AsyncUserToken
+    {
+        Socket m_socket;
+
+        public AsyncUserToken(Socket socket)
+        {
+            m_socket = socket;
+        }
+
+        public AsyncUserToken() : this(null) { }
+
+        public Socket Socket
+        {
+            get { return m_socket; }
+            set { m_socket = value; }
+        }
+    }
+}
---
+++ ezHttpConsoleVer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using SocketManager;
+using System.Net.Sockets;
+namespace ezHttpConsoleVer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numConnections;
+            int receiveSize;
+            IPEndPoint localEndPoint;
+            int port;
+
+
+            numConnections = 1024;
+            receiveSize = 64;
+            port = 8080;
+            localEndPoint = new IPEndPoint(IPAddress.Any, port);
+            // Start the server listening for incoming connection requests
+            Server server = new Server(numConnections, receiveSize);
+            server.Init();
+            server.Start(localEndPoint);
+            server.OnDataReceived += new Server.ConnetionChangedEventHandler(OnReceivedData);
+            Console.WriteLine("Press any key to terminate the server process....");
+            Console.ReadKey();
+        }
+
+        static void OnReceivedData(object sender, Socket e, byte[] data)
+        {
+
+            data = Encoding.Convert(Encoding.GetEncoding("iso-8859-1"), Encoding.UTF8, data);
+            string tempString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+            Console.WriteLine(tempString);
+            Console.WriteLine("Do anything you want");
+        }
+
+    }
+}
