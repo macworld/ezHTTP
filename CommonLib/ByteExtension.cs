fa11b4ed351dde4d980b552331a1707956563d15
Wenhao Liu <lwh.scu@gmail.com>
2014-03-16 18:56:46 +0800

    Add project framework & SocketManager Module Source code of the SocketManager  is from [MSDN Socket Performance Technology Sample] (http://goo.gl/ElRLZh) with slight changes.

---
+++ CommonLib/ByteExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CommonLib
+{
+    /// <summary>
+    /// Generic byte extension functions
+    /// </summary>
+    public static class ByteExtension
+    {
+
+        public static byte[] SubArray(this byte[] mydata, int index, int count)
+        {
+            if (count >= mydata.Length) return mydata;
+            byte[] message = new byte[count];
+            Buffer.BlockCopy(mydata, index, message, 0, count);
+            return message;
+        }
+
+        public static byte[] Concat(this byte[] data, byte[] val)
+        {
+            byte[] newdata = new byte[data.Length + val.Length];
+            Buffer.BlockCopy(data, 0, newdata, 0, data.Length);
+            Buffer.BlockCopy(val, 0, newdata, data.Length, val.Length);
+            return newdata;
+        }
+    }
+}
