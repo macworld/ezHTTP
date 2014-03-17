8174dd05c1c6d94881ee8024a3a3ece9c814c724
wox88 <wox88@qq.com>
2014-03-17 12:57:35 +0800

    添加log功能

---
+++ Log/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log
+{
+    public class Logger
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger("MyLogger");
+        private static readonly Logger interlog = new Logger();
+
+        public static Logger GetLogger()
+        {
+            return interlog;
+        }
+
+        public void Info(string msg)
+        {
+            log.Info(msg);
+        }
+    }
+}
