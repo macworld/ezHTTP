31da28b92d555d40b257d07b0fc185ba75753e5e
Wenhao Liu <lwh.scu@gmail.com>
2014-03-24 19:10:11 +0800

    添加了具有基本功能的HttpParser 目前已经能够在浏览器中显示HTML网页信息了，不过仍然还有很多事情要做

--- ezHttpConsoleVer/Program.cs
+++ ezHttpConsoleVer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Net;
+using HttpParser;
 using SocketManager;
 using System.Net.Sockets;
 namespace ezHttpConsoleVer
@@ -10,7 +11,8 @@ namespace ezHttpConsoleVer
     {
         static void Main(string[] args)
         {
-            SocketManagerExample();
+            //SocketManagerExample();
+            HttpParserExample();
         }
 
         static void SocketManagerExample()
@@ -34,5 +36,32 @@ namespace ezHttpConsoleVer
             Console.WriteLine("Do anything you want");
         }
 
+        static void HttpParserExample()
+        {
+
+            SocketServer socketServer = new SocketServer();
+            socketServer.Init();
+            if (socketServer.Start() == false)
+                Console.WriteLine("Start failed");
+            socketServer.OnDataReceived += new SocketServer.ConnetionChangedEventHandler(OnReceivedHttpReq);
+            Console.WriteLine("Press any key to terminate the server process....");
+            Console.ReadKey();
+        }
+        static void OnReceivedHttpReq(object sender, Socket e, byte[] data)
+        {
+
+
+            data = Encoding.Convert(Encoding.GetEncoding("iso-8859-1"), Encoding.UTF8, data);
+            int statusCode = 0;
+            HttpProtocolParser httpParser = new HttpProtocolParser(data);
+            if (httpParser.IsHttpRequest())
+            {
+                Console.WriteLine("\t\tresource url: " + httpParser.GetResourceUrl());
+                Byte[] sendData = FileManager.FileBuffer.GetInstance().readFile(httpParser.GetResourceUrl(), ref statusCode);
+                httpParser.SetStatusCode(statusCode);
+                e.Send(httpParser.GetWrappedResponse(sendData));
+            }
+
+        }
     }
 }
