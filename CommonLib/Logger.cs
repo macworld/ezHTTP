6e4b3ca72b6c9d008f9cf43688e77c869eab6e7d
wox88 <wox88@qq.com>
2014-04-14 23:14:27 +0800

    无用提交，多人修改Logger.cs冲突

--- CommonLib/Logger.cs
+++ CommonLib/Logger.cs
@@ -1,15 +1,121 @@
-﻿namespace CommonLib
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Log
 {
+    /// <summary>
+    /// 日志类定义，使用该日志类可以允许不同使用者输出到不同终端
+    /// 当前包含的终端：
+    /// 1. 使用者独有的Windows Form窗口，推荐调试使用
+    /// 2. 向特定窗口程序发送日志消息
+    /// 3. 其它log4net标准输出终端
+    /// </summary>
     public class Logger
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger("MyLogger");
-        private static readonly Logger interlog = new Logger();
-
-        private Logger()
+        /// <summary>
+        ///  默认的唯一构造函数
+        /// </summary>
+        /// <param name="loggerName">
+        /// 日志记录器Logger的名称
+        /// </param>
+        public  Logger(string loggerName)
         {
-            System.IO.FileInfo file = new System.IO.FileInfo("../../../CommonLib/Log.config");
+            LoggerName = loggerName;
+            InterLogger m_interlogger = InterLogger.GetIngerLogger();
+        }
 
-            if(file.Exists)
+        /// <summary>
+        /// 日志记录器Logger的名称，作为属性保存
+        /// </summary>
+        public string LoggerName
+        {
+            get { return m_loggerName; }
+            set { m_loggerName = value; }
+        }
+
+        /// <summary>
+        ///  输出Debug级别的日志消息
+        /// </summary>
+        /// <param name="debug">
+        /// 要输出的日志消息
+        /// </param>
+        public void Debug(string debug)
+        {
+            InterLogger.Debug(m_loggerName, debug);
+        }
+
+        /// <summary>
+        ///  输出Info级别的日志消息
+        /// </summary>
+        /// <param name="info">
+        /// 要输出的日志消息
+        /// </param>
+        public void Info(string info)
+        {
+            InterLogger.Info(m_loggerName, info);
+        }
+
+        /// <summary>
+        ///  输出Warn级别的日志消息
+        /// </summary>
+        /// <param name="warn">
+        /// 要输出的日志消息
+        /// </param>
+        public void Warn(string warn)
+        {
+            InterLogger.Warn(m_loggerName, warn);
+        }
+
+        /// <summary>
+        ///  输出Error级别的日志消息
+        /// </summary>
+        /// <param name="error">
+        /// 要输出的日志消息
+        /// </param>
+        public void Error(string error)
+        {
+            InterLogger.Error(m_loggerName, error);
+        }
+
+        /// <summary>
+        ///  输出Fatal级别的日志消息
+        /// </summary>
+        /// <param name="fatal">
+        /// 要输出的日志消息
+        /// </param>
+        public void Fatal(string fatal)
+        {
+            InterLogger.Fatal(m_loggerName, fatal);
+        }
+
+        /// <summary>
+        /// 属性对应的私有成员
+        /// </summary>
+        private string m_loggerName;
+    }
+
+    #region Logger类内部使用的实际用于日志输出的InterLogger类
+    /// <summary>
+    /// Logger类内部使用的实际用于日志输出的InterLogger类，static属性
+    /// </summary>
+    public class InterLogger
+    {
+        /// <summary>
+        /// InterLogger类的唯一不可见实例，只能通过GetLogger获得
+        /// </summary>
+        private static readonly InterLogger m_interlogger = new InterLogger();
+
+        /// <summary>
+        /// 构造类时，检查配置文件并按配置文件配置
+        /// </summary>
+        private  InterLogger()
+        {
+            System.IO.FileInfo file = new System.IO.FileInfo("../../Log.config");
+
+            if (file.Exists)
             {
                 log4net.Config.XmlConfigurator.ConfigureAndWatch(file);
             }
@@ -17,38 +123,90 @@
             {
                 System.Console.WriteLine("配置文件不存在");
             }
-
-            //log4net.Config.XmlConfigurator.ConfigureAndWatch();
         }
 
-        public static Logger GetLogger()
+        /// <summary>
+        ///  外部获得InterLogger类的唯一内部实例时使用的方法
+        /// </summary>
+        /// <returns>
+        ///  InterLogger类的唯一内部实例
+        /// </returns>
+        public static InterLogger GetIngerLogger()
         {
-            return interlog;
+            return m_interlogger;
         }
 
-        public void Debug(string debug)
+        /// <summary>
+        ///  输出Debug级别的日志消息
+        /// </summary>
+        /// <param name="logger">
+        /// 配置文件中，logger的name属性值
+        /// </param>
+        /// <param name="debug">
+        /// 要输出的Debug级别日志消息
+        /// </param>
+        public static void Debug(string logger, string debug)
         {
-            log.Debug(debug);
+            log4net.LogManager.GetLogger(logger).Debug(debug);
         }
 
-        public void Info(string info)
+        /// <summary>
+        ///  输出Info级别的日志消息
+        /// </summary>
+        /// <param name="logger">
+        /// 配置文件中，logger的name属性值
+        /// </param>
+        /// <param name="info">
+        /// 要输出的Info级别日志消息
+        /// </param>
+        public static void Info(string logger, string info)
         {
-            log.Info(info);
+            log4net.LogManager.GetLogger(logger).Info(info);
         }
 
-        public void Error(string err)
+        /// <summary>
+        ///  输出Warn级别的日志消息
+        /// </summary>
+        /// <param name="logger">
+        /// 配置文件中，logger的name属性值
+        /// </param>
+        /// <param name="warn">
+        /// 要输出的Warn级别日志消息
+        /// </param>
+        public static void Warn(string logger, string warn)
         {
-            log.Error(err);
+            log4net.LogManager.GetLogger(logger).Warn(warn);
         }
 
-        public void Warn(string warn)
+        /// <summary>
+        ///  输出Error级别的日志消息
+        /// </summary>
+        /// <param name="logger">
+        /// 配置文件中，logger的name属性值
+        /// </param>
+        /// <param name="error">
+        /// 要输出的Error级别日志消息
+        /// </param>
+        public static void Error(string logger, string error)
         {
-            log.Warn(warn);
+            log4net.LogManager.GetLogger(logger).Error(error);
         }
 
-        public void Fatal(string fatal)
+        /// <summary>
+        ///  输出Fatal级别的日志消息
+        /// </summary>
+        /// <param name="logger">
+        /// 配置文件中，logger的name属性值
+        /// </param>
+        /// <param name="fatal">
+        /// 要输出的Fatal级别日志消息
+        /// </param>
+        public static void Fatal(string logger, string fatal)
         {
-            log.Fatal(fatal);
+            log4net.LogManager.GetLogger(logger).Fatal(fatal);
         }
     }
+    #endregion Logger类内部使用的实际用于日志输出的InterLogger类
 }
+
+    
